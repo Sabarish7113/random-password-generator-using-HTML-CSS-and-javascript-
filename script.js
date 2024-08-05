fd2594082995f7243bf3dc54666b3b48b7cec648
Sabarish7113 <sabarish.user@gmail.com>
2024-08-05 15:28:02 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+document.getElementById('generateButton').addEventListener('click', function() {
+    const length = parseInt(document.getElementById('passwordLength').value);
+    const charset = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()_+~`|}{[]:;?><,./-=';
+    let password = '';
+    
+    for (let i = 0; i < length; i++) {
+        const randomIndex = Math.floor(Math.random() * charset.length);
+        password += charset[randomIndex];
+    }
+    
+    document.getElementById('resultContainer').innerText = `Your generated password is: ${password}`;
+});
